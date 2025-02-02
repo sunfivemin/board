402942bbad618570892c1ccaaa55c0bc0c42e428
sunfivemin <sunfivemin@gmail.com>
2025-02-02 13:08:19 +0900

    fix: bcrypt를 bcryptjs로 변경

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const methodOverride = require("method-override");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const MongoStore = require("connect-mongo");
 require("dotenv").config();
 
